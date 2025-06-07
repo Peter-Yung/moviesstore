4a56ad663c8793e57e3cc3518f467280b4c52ff0
Peter-Yung <peteryungcp@gmail.com>
2025-06-07 20:54:49 +0800

    Step 6 : Letting Users Create, Read, Update, and Delete.

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Review
 
 # Register your models here.
 # This class ordering movies by name.
@@ -8,3 +8,4 @@ class MovieAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Review)
--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,7 @@ app_name='movies' #Django 5.2 need this
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.show, name='show'),
+    path('<int:id>/review/create/', views.create_review, name='create_review'),
+    path('<int:id>/review/<int:review_id>/edit/', views.edit_review, name='edit_review'),
+    path('<int:id>/review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
 ]
